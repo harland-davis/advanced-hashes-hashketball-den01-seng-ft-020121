d04d069acb8850762bda9d28ec7c0ac2e4b0fdda
Kyle Davis <davis.h.kyle@gmail.com>
2021-01-27 23:12:10 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -210,7 +210,7 @@ end
 def big_shoe_rebounds
   game_hash.each do |location, team_data|
     team_data[:players].each do |player|
-     shoe_size = player[:shoe_size]
+     shoe_size = player[:shoe]
     
         binding.pry 
     end
